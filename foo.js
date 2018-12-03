1ab78391d0d132e46b1e9789cdc7663b83ea3385
Mike <mblack101@ufl.edu>
2018-12-02 21:17:06 -0500

    first commit

---
+++ foo.js
@@ -0,0 +1,5 @@
+var foo = function (bar) {
+    return bar + 1;
+};
+var dum = foo(5).toString();
+console.log(dum);
---
+++ foo.ts
@@ -0,0 +1,6 @@
+let foo = function(bar: number) : number {
+return bar + 1;
+}
+
+var dum = foo(5).toString();
+console.log(dum);
